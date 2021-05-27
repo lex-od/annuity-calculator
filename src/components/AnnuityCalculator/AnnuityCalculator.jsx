3439a93967d08335c5dcfdd879b5544c172455a1
Aleksey Yesin <esinru@gmail.com>
2021-05-27 06:28:10 +0300

    Добавляет формулу платежа, рефакторит AnnuityCalculator

--- src/components/AnnuityCalculator/AnnuityCalculator.jsx
+++ src/components/AnnuityCalculator/AnnuityCalculator.jsx
@@ -2,60 +2,51 @@ import { useState } from "react";
 import css from "./AnnuityCalculator.module.scss";
 import TwinInput from "../TwinInput";
 
-const MONTH_PAY_STEP = 50;
-const TERM_MIN = 1;
-const TERM_MAX = 30;
+const FEE_MIN_PART = 1 / 3;
+const RATE = 0.088 / 12;
 
 export default function AnnuityCalculator() {
-    const [monthPay, setMonthPay] = useState(100);
-    const [initPay, setInitPay] = useState(0);
+    const [pay, setPay] = useState(100);
+    const [fee, setFee] = useState(1000);
     const [term, setTerm] = useState(1);
+    const [cost, setCost] = useState(3000);
 
-    const handleSetMonthPay = (e) => {
-        setMonthPay(Number(e.target.value));
+    const handleSetPay = (e) => {
+        setPay(Number(e.target.value));
     };
 
-    const handleSetInitPay = (e) => {
-        setInitPay(Number(e.target.value));
+    const handleSetFee = (e) => {
+        setFee(Number(e.target.value));
     };
 
     const handleSetTerm = (e) => {
         setTerm(Number(e.target.value));
     };
 
-    let monthPayMin = 100;
-    let monthPayMax = 10000;
-
-    let initPayMin = 0;
-    let initPayMax = 10000;
-
-    let cost = 1144;
+    const payTmp =
+        (cost * RATE * (1 + RATE) ** (term * 12)) /
+        ((1 + RATE) ** (term * 12) - 1);
 
     return (
         <div className={css.annuityCalculator}>
             <TwinInput
-                value={monthPay}
-                onChange={handleSetMonthPay}
-                min={monthPayMin}
-                max={monthPayMax}
-                step={MONTH_PAY_STEP}
+                value={pay}
+                onChange={handleSetPay}
+                min={100}
+                max={10000}
+                step={50}
             />
 
             <TwinInput
-                value={initPay}
-                onChange={handleSetInitPay}
-                min={initPayMin}
-                max={initPayMax}
+                value={fee}
+                onChange={handleSetFee}
+                min={Math.round(cost * FEE_MIN_PART)}
+                max={cost}
             />
 
-            <TwinInput
-                value={term}
-                onChange={handleSetTerm}
-                min={TERM_MIN}
-                max={TERM_MAX}
-            />
+            <TwinInput value={term} onChange={handleSetTerm} min={1} max={30} />
 
-            <p>Стоимость: ${cost}</p>
+            <p>Стоимость: ${payTmp}</p>
         </div>
     );
 }
